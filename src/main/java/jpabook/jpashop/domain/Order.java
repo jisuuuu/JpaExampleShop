9210612435c9d0e71867f71cfb0cdcab9fe7b573
Jisu Choi <gjkl55@naver.com>
2022-06-21 20:19:19 +0900

    [삭제] 엔티티 클래스 개발을 위한 기존 Member 클래스 삭제

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+}
