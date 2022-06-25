eea4e5c873ef06240d6b86b56b596393d79e0fe8
Jisu Choi <gjkl55@naver.com>
2022-06-25 20:19:19 +0900

    [수정] ManyToOne, OneToOne 지연로딩으로 변경

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,14 +17,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY) //추적이 어려움 LAZY(지연로딩)으로 바꿔야함 아니면 전부 실행
     @JoinColumn(name = "member_id")
     private Member member;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
